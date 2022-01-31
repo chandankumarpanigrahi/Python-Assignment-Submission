ac956b3746d789f968867c7fce2f00b472b2bbbb
Chandan Kumar Panigrahi <91644974+chandankumarpanigrahi@users.noreply.github.com>
2022-01-31 21:35:44 +0530

    Day 1 Assignment Submitted

---
+++ Python Day 1 Assignment/Day1.py
@@ -0,0 +1,7 @@
+#Take multiple values as input and print sum of them.
+a=int(input('Enter The First Value:'))
+b=int(input('Enter The Second Value:'))
+c=int(input('Enter The Third Value:'))
+d=int(input('Enter The Fourth Value:'))
+e=int(input('Enter The Fifth Value:'))
+print('The Sum Of',a,',',b,',',c,',',d,',',e,' is:',a+b+c+d+e)
